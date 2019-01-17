bad3486bda79a36ce8b758f5481f5b869d871036
Israel Calvete <icalvete@gmail.com>
2019-01-17 17:23:00 +0100

    Fix issue.

--- manifests/postconfig.pp
+++ manifests/postconfig.pp
@@ -3,7 +3,7 @@ class sonar::postconfig {
   include mysql::client
 
   exec {'set_sonar_admin_pass':
-    command  => "echo \"UPDATE users SET crypted_password=SHA1(CONCAT('--', salt, '--', '${sonar::admin_pass}\', '--')) WHERE login='admin'\" | /usr/bin/mysql -h ${sonar::db_host} -u${sonar::db_user} ${sonar::db} -p${sonar::db_pass}",
+    command  => "echo \"UPDATE users SET crypted_password=SHA1(CONCAT('--', salt, '--', '${sonar::admin_pass}\', '--')) WHERE login='admin';\" | /usr/bin/mysql -h ${sonar::db_host} -u${sonar::db_user} ${sonar::db} -p${sonar::db_pass}",
     provider => 'shell',
     user     => 'root',
     require  => Class['mysql::client']
